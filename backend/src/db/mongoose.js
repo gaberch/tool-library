bd61b139d81a1d5dfdb2deb5229f759d32fe5171
Gabe Richard <gabe_rch@hotmail.com>
2020-03-21 12:14:19 -0400

    Added new db connection to cloud hosted mongoDB on Atlas instead of local db

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/tool-library', {
+mongoose.connect('mongodb+srv://admin:admin@cluster0-ytkv7.mongodb.net/tool-library?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
