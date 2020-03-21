3bcda4f0cf5b605bd96b0fbec0ee8d27ced12db2
Gabe Richard <gabe_rch@hotmail.com>
2020-03-21 11:59:50 -0400

    Added individual get tool api and update individual tool api

--- backend/src/routers/tool.js
+++ backend/src/routers/tool.js
@@ -15,5 +15,56 @@ toolRouter.post('/tools', async (req, res) => {
     }
 });
 
+toolRouter.get('/tools', async (req, res) => {
+    try {
+        const tools = await Tool.find({});
+
+        res.send(tools);
+    } catch (error) {
+        res.status(500).send();
+    }
+
+});
+
+toolRouter.get('/tools/:id', async (req, res) => {
+    const _id = req.params.id;
+    
+    try {
+        const tool = await Tool.findById(_id);
+
+        if(!tool) {
+            return res.status(404).send();
+        }
+
+        res.send(tool);
+    } catch(error) {
+        res.status(500).send();
+    }
+})
+
+toolRouter.patch('/tools/:id', async (req, res) => {
+    const _id = req.params.id;
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'description', 'available'];
+    const validUpdate = updates.every((update) => allowedUpdates.includes(update));
+
+    if(!validUpdate) {
+        return res.status(400).send({error: "Invalid update parameter"})
+    }
+
+    try {
+        const tool = await Tool.findByIdAndUpdate(_id, { runValidators: true, new:true })
+
+        if(!tool) {
+            return res.status(404).send();
+        }
+
+        res.send(tool);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+})
+
 
 module.exports = toolRouter;
